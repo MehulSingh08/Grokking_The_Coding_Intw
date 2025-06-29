6f9f689706dad1f740d7a2a621f18ab7d2e8beeb
Metagrammer <113834047+Metagrammer@users.noreply.github.com>
2025-06-29 15:47:07 +0530

    nrc

---
+++ HashMap/nonrepeat.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class nonrepeat
+{
+    public static Character nrc(String s)
+    {
+        Map<Character, Integer> hash = new LinkedHashMap<>();
+
+        for(char c : s.toCharArray())
+        {
+            if(hash.containsKey(c))
+            {
+                hash.put(c, hash.get(c) +1);
+            }
+            else
+            {
+                hash.put(c,1);
+            }
+        }
+
+        for(Map.Entry<Character, Integer> entry : hash.entrySet())
+        {
+            if(entry.getValue() == 1)
+            {
+                return entry.getKey();
+            }
+        }
+        return null;
+
+
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Character result = nrc(s);
+       // System.out.println(result);
+        if (result != null) {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+    }
+}
